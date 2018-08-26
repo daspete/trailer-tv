a31bd6fc06921fce62dfa04168691f51d2048f97
daspete <daspetemail@gmail.com>
2018-08-26 18:35:05 +0200

    poster protocol change

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-sm-8 col-md-3 col-lg-3">
                 <div class="movie-card__poster"
-                    :style="`background-image: url(${ movie.poster })`"
+                    :style="`background-image: url(${ movie.poster.replace('http:', '') })`"
                 ></div>
                 <div class="movie-card__actions">
                     
